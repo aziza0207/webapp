d9b7e4960d8968f16e31e0cc81c229c37d3e4b04
Aziza <azizamukashkyzy@gmail.com>
2023-09-04 12:40:56 +0600

    multilanguage functionality added

--- web/views.py
+++ web/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from django.utils import translation
 from .models import (Products,
                      Practice,
                      Principle,
@@ -20,32 +21,68 @@ from .serializers import (ProductsSerializer,
 
 class ProductsListView(generics.ListAPIView):
     serializer_class = ProductsSerializer
-    queryset = Products.objects.all()
+
+    def get_queryset(self):
+        queryset = Products.objects.all()
+        if 'HTTP_ACCEPT_LANGUAGE' in self.request.META:
+            lang = self.request.META['HTTP_ACCEPT_LANGUAGE']
+            translation.activate(lang)
+        return queryset
 
 
 class PracticeListView(generics.ListAPIView):
     serializer_class = PracticeSerializer
-    queryset = Practice.objects.all()
+
+    def get_queryset(self):
+        queryset = Practice.objects.all()
+        if 'HTTP_ACCEPT_LANGUAGE' in self.request.META:
+            lang = self.request.META['HTTP_ACCEPT_LANGUAGE']
+            translation.activate(lang)
+        return queryset
 
 
 class PrincipleListView(generics.ListAPIView):
     serializer_class = PrincipleSerializer
-    queryset = Principle.objects.all()
+
+    def get_queryset(self):
+        queryset = Principle.objects.all()
+        if 'HTTP_ACCEPT_LANGUAGE' in self.request.META:
+            lang = self.request.META['HTTP_ACCEPT_LANGUAGE']
+            translation.activate(lang)
+        return queryset
 
 
 class ExperienceListView(generics.ListAPIView):
     serializer_class = ExperienceSerializer
-    queryset = Experience.objects.all()
+
+    def get_queryset(self):
+        queryset = Experience.objects.all()
+        if 'HTTP_ACCEPT_LANGUAGE' in self.request.META:
+            lang = self.request.META['HTTP_ACCEPT_LANGUAGE']
+            translation.activate(lang)
+        return queryset
 
 
 class ContactsListView(generics.ListAPIView):
     serializer_class = ContactsSerializer
-    queryset = Contacts.objects.all()
+
+    def get_queryset(self):
+        queryset = Contacts.objects.all()
+        if 'HTTP_ACCEPT_LANGUAGE' in self.request.META:
+            lang = self.request.META['HTTP_ACCEPT_LANGUAGE']
+            translation.activate(lang)
+        return queryset
 
 
 class ValueListView(generics.ListAPIView):
     serializer_class = ValueSerializer
-    queryset = Value.objects.all()
+
+    def get_queryset(self):
+        queryset = Value.objects.all()
+        if 'HTTP_ACCEPT_LANGUAGE' in self.request.META:
+            lang = self.request.META['HTTP_ACCEPT_LANGUAGE']
+            translation.activate(lang)
+        return queryset
 
 
 class PhotoView(generics.ListAPIView):
